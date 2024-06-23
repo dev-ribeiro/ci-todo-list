9710b1bfe369b06e1dcab392bb5bd7027d751a53
David Ribeiro da Silva <david.ribeiro.professional@gmail.com>
2024-06-23 11:28:59 -0300

    config: setup bootstrap

--- application/views/home.php
+++ application/views/home.php
@@ -1,11 +1,16 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<title>Home | Todo List</title>
+	<link rel="stylesheet" href="<?php echo base_url('bootstrap/css/bootstrap.min.css') ?>">
+	<script src="<?php echo base_url('bootstrap/js/bootstrap.min.js') ?>" defer></script>
 </head>
+
 <body>
 	<h1>Bem vindo a aplicação todo list</h1>
 </body>
+
 </html>
